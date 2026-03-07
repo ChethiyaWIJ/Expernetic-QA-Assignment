9740fb4cb1951171ef3e76861f033ad00ccc2e1c
ChethiyaWIJ <wijerathne.chethiya@gmail.com>
2026-03-07 15:39:40 +0530

    add factorial method fully

--- src/main/java/algorithmDevelopment/FactorialCalculator.java
+++ src/main/java/algorithmDevelopment/FactorialCalculator.java
@@ -1,4 +1,39 @@
 package algorithmDevelopment;
+import java.util.Scanner;
 
 public class FactorialCalculator {
+
+    // Function to calculate factorial
+    public static long factorial(int n) {
+        long result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+
+        return result;
+    }
+
+    //main method
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+
+        //getting user input
+        while (true) {
+            System.out.print("Enter a non-negative integer: ");
+            n = scanner.nextInt();
+
+            if (n >= 0) {
+                break;
+            } else {
+                System.out.println("Invalid input. Please enter a non-negative integer.");
+            }
+        }
+        //printing the output
+        long fact = factorial(n);
+        System.out.println("Factorial of " + n + " is: " + fact);
+
+        scanner.close();
+    }
 }
